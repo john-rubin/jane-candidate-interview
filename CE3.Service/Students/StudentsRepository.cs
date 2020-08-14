8d63161724c41277c136f18ecea357f4198343f8
Garrett Roberts <garrett@thevoid.com>
2020-08-14 11:18:39 -0600

    Interview work

--- CE3.Service/Students/StudentsRepository.cs
+++ CE3.Service/Students/StudentsRepository.cs
@@ -29,19 +29,36 @@ namespace CE3.Service.Students
 			throw new NotImplementedException();
 		}
 
-		public Task<Student> CreateStudent(Student student)
+		public async Task<Student> CreateStudent(Student student)
 		{
-			throw new NotImplementedException();
+			using (var context = _universityDbFactory.GetDbContext())
+			{
+				var dbStudent = await GetStudent(student.FirstName, student.LastName);
+				if (dbStudent != null)
+					return dbStudent;
+
+				dbStudent = context.Students.Add(student);
+				await context.SaveChangesAsync();
+				return dbStudent;
+			}
 		}
 
 		public Task<Student> GetStudent(int id)
 		{
-			throw new NotImplementedException();
+			using (var context = _universityDbFactory.GetDbContext())
+			{
+				return context.Students.FirstOrDefaultAsync(s => s.Id == id);
+			}
 		}
 
 		public Task<Student> GetStudent(string firstName, string lastName)
 		{
-			throw new NotImplementedException();
+			using (var context = _universityDbFactory.GetDbContext())
+			{
+				return context.Students.FirstOrDefaultAsync(s =>
+					s.FirstName.Equals(firstName, StringComparison.InvariantCultureIgnoreCase) &&
+					s.LastName.Equals(lastName, StringComparison.InvariantCultureIgnoreCase));
+			}
 		}
 	}
 }
