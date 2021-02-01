a1a386c6e1073203b22ff5405d5129152db68e7a
Ben Allred <ben@bmallred.com>
2021-02-01 11:34:35 -0700

    Implement StudentsRepository

--- CE3.Service/Students/StudentsRepository.cs
+++ CE3.Service/Students/StudentsRepository.cs
@@ -24,19 +24,51 @@ namespace CE3.Service.Students
 			_universityDbFactory = universityDbFactory;
 		}
 
-		public Task<Student> ChangeLastName(int id, string newLastName)
+		public async Task<Student> ChangeLastName(int id, string newLastName)
 		{
-			throw new NotImplementedException();
+			using (var dbContext = _universityDbFactory.GetDbContext())
+			{
+				var foundStudent = await dbContext.Students.SingleOrDefaultAsync(student => student.Id == id);
+				if (foundStudent != null)
+				{
+					foundStudent.LastName = newLastName;
+					await dbContext.SaveChangesAsync();
+				}
+				return foundStudent;
+			}
 		}
 
-		public Task<Student> CreateStudent(Student student)
+		public async Task<Student> CreateStudent(Student student)
 		{
-			throw new NotImplementedException();
+			using (var dbContext = _universityDbFactory.GetDbContext())
+			{
+				var addedStudent = dbContext.Students.Add(student);
+				await dbContext.SaveChangesAsync();
+				return addedStudent;
+			}
 		}
 
 		public Task<Student> GetStudent(int id)
 		{
-			throw new NotImplementedException();
+			return DoQuery(async dbContext =>
+			{
+				var foundStudent = await dbContext.Students.SingleOrDefaultAsync(student => student.Id == id);
+				return foundStudent;
+			});
+			//using (var dbContext = _universityDbFactory.GetDbContext())
+			//{
+			//	var foundStudent = await dbContext.Students.SingleOrDefaultAsync(student => student.Id == id);
+			//	return foundStudent;
+			//}
+		}
+
+		private async Task<Student> DoQuery(Func<IUniversityDbContext, Task<Student>> query)
+		{
+			using (var dbContext = _universityDbFactory.GetDbContext())
+			{
+				var student = await query(dbContext);
+				return student;
+			}
 		}
 
 		public Task<Student> GetStudent(string firstName, string lastName)
