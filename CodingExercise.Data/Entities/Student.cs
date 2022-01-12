e08cebbec69235d0359a68bb3378e87a6d8ffe16
John Rubin <mysport50@gmail.com>
2022-01-12 12:23:57 -0700

    add key, global using

--- CodingExercise.Data/Entities/Student.cs
+++ CodingExercise.Data/Entities/Student.cs
@@ -1,7 +1,9 @@
 ﻿namespace CodingExercise.Data.Entities
 {
+	[Table("students")]
 	public class Student
 	{
+		[Key]
 		public int Id { get; set; }
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
--- CodingExercise.Data/GlobalUsing.cs
+++ CodingExercise.Data/GlobalUsing.cs
@@ -1,6 +1,8 @@
 ﻿global using System;
 global using System.Linq;
 global using System.Collections.Generic;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
 
 global using Microsoft.EntityFrameworkCore;
 
